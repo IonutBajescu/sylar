54d3f83cdc642821d0c1f4a89d02395a6e52860b
Ionut Bajescu <ionutbajescu@gmail.com>
2014-09-06 03:10:40 +0300

    fix autoload path

--- src/Ionut/SecurityListener/autoload.php
+++ src/Ionut/SecurityListener/autoload.php
@@ -1,10 +1,11 @@
 <?php
 
 $base = dirname(dirname(dirname(__DIR__)));
+$vendorDir = dirname(dirname($base));
 
 if (file_exists($file = $base . '/vendor/autoload.php')) {
 	include $file;
-} elseif (file_exists($file = $base . '/vendor/autoload.php')) {
+} elseif (file_exists($file = $vendorDir . '/autoload.php')) {
 	include $file;
 } else {
 	throw new Exception("You don't have an autoload.");
